5b91783d3bda81a2e654054e75d3e103012aa6fc
raulshma <raulshma@gmail.com>
2020-08-13 02:26:04 +0530

    Updates

---
+++ precache-manifest.1cae3991c8eca4f913cd8f0de57f86a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb53376a5ee9f7295cfa6f41a81334a9",
+    "url": "/bitcoin-price-chart/index.html"
+  },
+  {
+    "revision": "213007018c469606fe41",
+    "url": "/bitcoin-price-chart/static/css/2.0a1d61fb.chunk.css"
+  },
+  {
+    "revision": "2a68d758911ec79e2dcd",
+    "url": "/bitcoin-price-chart/static/css/main.fdfbfd47.chunk.css"
+  },
+  {
+    "revision": "213007018c469606fe41",
+    "url": "/bitcoin-price-chart/static/js/2.a22e78c7.chunk.js"
+  },
+  {
+    "revision": "16353a103b2d86eb32c8c5b96fa4a77b",
+    "url": "/bitcoin-price-chart/static/js/2.a22e78c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a68d758911ec79e2dcd",
+    "url": "/bitcoin-price-chart/static/js/main.25ee0ff1.chunk.js"
+  },
+  {
+    "revision": "ffc94d9fccd40abb276b",
+    "url": "/bitcoin-price-chart/static/js/runtime-main.0ac84a8b.js"
+  }
+]);
